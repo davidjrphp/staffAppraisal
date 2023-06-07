8cfb92dd0e0dc62ecf0297e36697391859742477
davidjrphp <davidgarciajr955@gmail.com>
2023-06-07 07:29:12 -0600

    publish the project

---
+++ database/appraisal.sql
@@ -0,0 +1,355 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 06, 2023 at 03:11 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appraisal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department_list`
+--
+
+CREATE TABLE `department_list` (
+  `id` int(30) NOT NULL,
+  `department` varchar(200) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `department_list`
+--
+
+INSERT INTO `department_list` (`id`, `department`, `description`) VALUES
+(1, 'IT Department', 'Information Technology Department'),
+(2, 'ART ', 'ART Department'),
+(3, 'M&E', 'Monitoring & Evaluation Department'),
+(4, 'DSD', 'DSD Department');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designation_list`
+--
+
+CREATE TABLE `designation_list` (
+  `id` int(30) NOT NULL,
+  `designation` varchar(200) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `designation_list`
+--
+
+INSERT INTO `designation_list` (`id`, `designation`, `description`) VALUES
+(1, 'Assistant M&E', 'Assistant Monitoring & Evaluation Officer'),
+(2, 'EHRO', 'Electronic Health Records Officer'),
+(3, 'M&E Officer', 'Monitoring & Evaluation Officer'),
+(4, 'Dr', 'Medical Doctor'),
+(5, 'Data Associate', 'Data Associate'),
+(6, 'CO', 'Clinical Officer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_list`
+--
+
+CREATE TABLE `employee_list` (
+  `id` int(30) NOT NULL,
+  `employee_id` varchar(50) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `middlename` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `department_id` int(30) NOT NULL,
+  `designation_id` int(30) NOT NULL,
+  `evaluator_id` int(30) NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employee_list`
+--
+
+INSERT INTO `employee_list` (`id`, `employee_id`, `firstname`, `middlename`, `lastname`, `email`, `password`, `department_id`, `designation_id`, `evaluator_id`, `avatar`, `date_created`) VALUES
+(3, '', 'Sandra', 'Kabuswe', 'Moyo', 'sandramoyo@gmail.com', '3fc5586bed4fb9f745500c0605197924', 3, 3, 2, 'no-image-available.png', '2023-05-31 14:42:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evaluator_list`
+--
+
+CREATE TABLE `evaluator_list` (
+  `id` int(30) NOT NULL,
+  `employee_id` varchar(50) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `middlename` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `evaluator_list`
+--
+
+INSERT INTO `evaluator_list` (`id`, `employee_id`, `firstname`, `middlename`, `lastname`, `email`, `password`, `avatar`, `date_created`) VALUES
+(2, '', 'Maurice', '', 'Manzi', 'mauricemanzi@yahoo.com', '89c2aea4392de83c2694ff08b10fce05', '1685565660_1607134440_avatar.jpg', '2023-05-31 14:41:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+CREATE TABLE `ratings` (
+  `id` int(30) NOT NULL,
+  `employee_id` int(30) NOT NULL,
+  `task_id` int(30) NOT NULL,
+  `evaluator_id` int(30) NOT NULL,
+  `efficiency` int(11) NOT NULL,
+  `timeliness` int(11) NOT NULL,
+  `quality` int(11) NOT NULL,
+  `accuracy` int(11) NOT NULL,
+  `remarks` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ratings`
+--
+
+INSERT INTO `ratings` (`id`, `employee_id`, `task_id`, `evaluator_id`, `efficiency`, `timeliness`, `quality`, `accuracy`, `remarks`, `date_created`) VALUES
+(2, 1, 1, 1, 5, 4, 5, 5, 'Sample', '2020-12-05 15:18:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'Smart Appraisal', 'info@moh.com', '+260 972862797', 'Ministry of Health, Zambia', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_list`
+--
+
+CREATE TABLE `task_list` (
+  `id` int(30) NOT NULL,
+  `task` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `employee_id` int(30) NOT NULL,
+  `due_date` date NOT NULL,
+  `completed` date NOT NULL,
+  `status` int(1) NOT NULL COMMENT '0=pending, 1=on-progress,3=Completed',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_progress`
+--
+
+CREATE TABLE `task_progress` (
+  `id` int(11) NOT NULL,
+  `task_id` int(30) NOT NULL,
+  `progress` text NOT NULL,
+  `is_complete` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0=no,1=Yes',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `task_progress`
+--
+
+INSERT INTO `task_progress` (`id`, `task_id`, `progress`, `is_complete`, `date_created`) VALUES
+(1, 1, '&lt;p&gt;Sample Progress&lt;/p&gt;', 0, '2020-12-05 11:29:48'),
+(2, 1, '&lt;p&gt;Sample Progress&lt;/p&gt;', 0, '2020-12-05 11:32:15'),
+(3, 1, '&lt;p&gt;Sample 2&lt;/p&gt;', 0, '2020-12-05 11:34:18'),
+(4, 1, 'asdasd', 0, '2020-12-05 11:34:31'),
+(5, 1, '&lt;p&gt;complete&lt;/p&gt;', 1, '2020-12-05 11:54:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `avatar`, `date_created`) VALUES
+(1, 'David', 'Mwelwa', 'davidgarciajr955@gmail.com', '55fc5b709962876903785fd64a6961e5', '1607135820_avatar.jpg', '2020-11-26 10:57:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `department_list`
+--
+ALTER TABLE `department_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `designation_list`
+--
+ALTER TABLE `designation_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee_list`
+--
+ALTER TABLE `employee_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `evaluator_list`
+--
+ALTER TABLE `evaluator_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task_list`
+--
+ALTER TABLE `task_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task_progress`
+--
+ALTER TABLE `task_progress`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `department_list`
+--
+ALTER TABLE `department_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `designation_list`
+--
+ALTER TABLE `designation_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `employee_list`
+--
+ALTER TABLE `employee_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `evaluator_list`
+--
+ALTER TABLE `evaluator_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `task_list`
+--
+ALTER TABLE `task_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `task_progress`
+--
+ALTER TABLE `task_progress`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
